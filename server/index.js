90add7e7f3c5d828132daddcae63db3d33e4d609
atheduc <athaneduc@gmail.com>
2024-12-23 15:27:57 +0800

    https://carolink-webdev.vercel.app

--- server/index.js
+++ server/index.js
@@ -67,7 +67,7 @@ const app = express();
 // CORS Configuration - Update for correct origins in production
 const corsOptions = {
   origin: process.env.NODE_ENV === 'production' 
-    ? 'https://caro-link.vercel.app'   // Frontend URL for production
+    ? 'https://carolink-webdev.vercel.app'   // Frontend URL for production
     : 'http://localhost:3000',  // Local development URL
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'accessToken'],
@@ -150,7 +150,7 @@ app.use("/likes", likesRouter);
 
 // Redirect the root (/) to the frontend on Vercel
 app.get('/', (req, res) => {
-  res.redirect('https://caro-link.vercel.app');  // Update with your Vercel URL
+  res.redirect('https://carolink-webdev.vercel.app');  // Update with your Vercel URL
 });
 
 // Catch-all route handler for undefined routes
